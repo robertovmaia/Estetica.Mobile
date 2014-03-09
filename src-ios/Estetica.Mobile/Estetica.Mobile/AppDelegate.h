b59ef028e503ee203d96a08bd28ec09dcb258bef
Fernando Delucca <fernando.delucca@live.com>
2014-03-09 16:34:20 -0300

    .

---
+++ src-ios/Estetica.Mobile/Estetica.Mobile/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Estetica.Mobile
+//
+//  Created by Fernando Delucca on 3/9/14.
+//  Copyright (c) 2014 Fernando Delucca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ src-ios/Estetica.Mobile/Estetica.MobileTests/Estetica_MobileTests.m
@@ -0,0 +1,34 @@
+//
+//  Estetica_MobileTests.m
+//  Estetica.MobileTests
+//
+//  Created by Fernando Delucca on 3/9/14.
+//  Copyright (c) 2014 Fernando Delucca. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Estetica_MobileTests : XCTestCase
+
+@end
+
+@implementation Estetica_MobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
